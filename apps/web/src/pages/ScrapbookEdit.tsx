218cd48a4c3c449d15c8b9181682764fed4e9b09
NisaMeshal <nisameshal@gmail.com>
2026-01-03 22:04:36 -0800

    page setup

---
+++ apps/web/src/pages/ScrapbookEdit.tsx
@@ -0,0 +1,14 @@
+//token read logic
+// import { useParams, useSearchParams } from "react-router-dom";
+
+// const { id } = useParams();
+// const [searchParams] = useSearchParams();
+
+// const token = searchParams.get("token");
+
+
+function ScrapbookEdit() {
+    return (<></>)
+}
+
+export default ScrapbookEdit
---
+++ apps/web/src/pages/ScrapbookView.tsx
@@ -0,0 +1,5 @@
+function ScrapbookView() {
+    return(<></>)
+}
+
+export default ScrapbookView
